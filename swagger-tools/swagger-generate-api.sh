79392f3b20a6e172e2ffaff166242b81745cd160
Simone Chemelli <simone.chemelli@gmail.com>
2023-11-22 18:33:16 +0000

    build: swagger tool setup

---
+++ swagger-tools/swagger-generate-api.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+java -jar ./swagger/swagger-codegen-cli.jar generate \
+   -i https://dev.fitbit.com/build/reference/web-api/explore/fitbit-web-api-swagger.json \
+   -l python \
+   -o .
