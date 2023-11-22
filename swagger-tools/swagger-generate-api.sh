315cf7d46930d8a0cba6764dfbfc54288567600c
Simone Chemelli <simone.chemelli@gmail.com>
2023-11-22 18:44:56 +0000

    chore: rename project after refresh

--- swagger-tools/swagger-generate-api.sh
+++ swagger-tools/swagger-generate-api.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-java -jar ./swagger/swagger-codegen-cli.jar generate \
+java -jar ./swagger-tools/swagger-codegen-cli.jar generate \
    -i https://dev.fitbit.com/build/reference/web-api/explore/fitbit-web-api-swagger.json \
    -l python \
    -o .
+
+# Restore projetc name after automatic file update
+sed -i -e 's/swagger-client/fitbit-web-api/g' setup.py
